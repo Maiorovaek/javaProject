0ef3ea2947d73e8298839aff545fd7ca79873f4b
Maiorovaek <katushkm@yandex.ru>
2019-02-05 05:23:39 +0300

    adding operation  update,add,delete,find,getAll. Start javafx

---
+++ RMIProject/src/main/java/rmi/RMIClient.java
@@ -0,0 +1,234 @@
+package rmi;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import rmi.dao.ICrudCollection;
+import rmi.model.Student;
+import rmi.model.Student.Department;
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class RMIClient extends Application {
+    ICrudCollection collectiond;
+
+    public RMIClient() throws IOException {
+
+       // try {
+            init();
+          //  mainLoop();
+      //  } catch (IOException ex) {
+          //  System.out.println(ex.getMessage());
+        //}
+    }
+
+     public void init() {
+
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            Registry registry = LocateRegistry.getRegistry();
+            collectiond = (ICrudCollection) registry.lookup("MyRemote");
+
+        } catch (Throwable cause) {
+            System.err.println("" + cause.getMessage());
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       new RMIClient();
+        primaryStage.setTitle("RMI GUI Client API");
+
+        VBox root = new VBox(5);
+        Label lblNumber = new Label("Gradebook number:  ");
+        TextField idlblNumberG =new TextField();
+        Button btnGetAll = new Button("Get all list students");
+        Button btnAddProcess = new Button("Add");
+        Button btnDelProcess = new Button("Delete");
+        Button btnUpdateProcess = new Button("Update");
+        Button btnfindGradebook = new Button("Find gradebook");
+        Button btnfindName = new Button("Find name");
+
+
+        Button btnfindSurname = new Button("Find surname");
+        Button btnfindDepartment = new Button("Find Department");
+        Button btnfindAvScore = new Button("Find Average Score");
+        Label lblResponse = new Label();
+
+        btnAddProcess.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try{
+                    String response = "ddsv";
+                    lblResponse.setText(response);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        root.getChildren().addAll( lblNumber, lblResponse, idlblNumberG, btnGetAll, btnAddProcess, btnDelProcess,btnUpdateProcess,btnfindGradebook,btnfindName,btnfindSurname,btnfindDepartment,btnfindAvScore);
+
+       primaryStage.setScene(new Scene(root, 300, 500));
+        primaryStage.show();
+    }
+
+    private void mainLoop() throws IOException {
+        char choice = 100;
+        System.out.println("0. Exit");
+        System.out.println("1. Get All Students");
+        System.out.println("2. Add New Student");
+        System.out.println("3. Find student's surname");
+        System.out.println("4. Find student's name");
+        System.out.println("5. Find student's departament");
+        System.out.println("6. Find student's gradebook Number");
+        System.out.println("7. who have score greater than entered ");
+        System.out.println("8. delete to student's gradebook Number ");
+        System.out.println("9. Update student ");
+
+        System.out.println("Введите пункт меню и нажмите Enter:");
+        do {
+            choice = (char) System.in.read();
+
+            switch (choice) {
+                case '1':
+                    printAllStudents();
+                    break;
+                case '2':
+                    addStudent();
+                    break;
+                case '3':
+                    findSurname();
+                    break;
+                case '4':
+                    findByName();
+                    break;
+                case '5':
+                    findByDepartment();
+                    break;
+                case '6':
+                    findByGradebookNumber();
+                    break;
+                case '7':
+                    findWhosScoreGreater();
+                    break;
+                case '8':
+                    removeStudent();
+                    break;
+                case '9':
+                    updateStudent();
+                    break;
+            }
+        }
+        while (choice != '0');
+    }
+
+
+    private void addStudent() throws RemoteException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите номер зачетной книжки ");
+        String numberS = in.next();
+        long number = Integer.parseInt(numberS);
+        System.out.println("Введите имя студента");
+       String name = in.next();
+        System.out.println("Введите фамилию студента");
+        String surname = in.next();
+        System.out.println("Введите отделение: ");
+        System.out.println("существуют" + Arrays.toString(Department.values()));
+        String d = in.next();
+        Department subject = Department.valueOf(d);
+        System.out.println("Введите оценку");
+        String m = in.next();
+        double minScore = Double.parseDouble(m);
+        Student s = new Student(number, name, surname, subject, minScore);
+        collectiond.addSt(s);
+        System.out.println("Добавлен студент " + s);
+    }
+
+    private void printAllStudents() throws RemoteException {
+        System.out.println("Список студентов: " + collectiond.getAll());
+    }
+
+    private void findSurname() throws IOException {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите фамилию: ");
+        String surname = in.next();
+
+        System.out.println("Найти по фамилии" + collectiond.findBySurname(surname));
+    }
+
+    private void findByName() throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = in.next();
+
+        System.out.println("Найти по имени" + collectiond.findByName(name));
+    }
+
+    private void findByDepartment() throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите отделение: ");
+        String d = in.next();
+        Department high = Department.valueOf(d);
+        System.out.println("Найти по отделению" + collectiond.findByDepartment(high));
+
+    }
+
+    private void findByGradebookNumber() throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите номер зачетной книжки ");
+        String number = in.next();
+        long high = Integer.parseInt(number);
+        System.out.println("Студент " + collectiond.findByGradebookNumber(high));
+    }
+
+    private void findWhosScoreGreater() throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите оценку, чтобы найти студентов у которых балл выше введенной оценки ");
+        String m = in.next();
+        double minScore = Double.parseDouble(m);
+
+        System.out.println("Студенты " + collectiond.findWhosScoreGreater(minScore));
+    }
+
+    private void removeStudent() throws IOException {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите номер зачетной книжки для удаления студента ");
+        String number = in.next();
+        long high = Integer.parseInt(number);
+        collectiond.removeStudent(high);
+    }
+
+
+    private void updateStudent() throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите номер зачетной книжки ");
+        String idg = in.next();
+        Long ids = Long.valueOf(idg);
+        System.out.print("Введите обновленный балл");
+        String averageScore = in.next();
+        Double averageScoreD = Double.parseDouble(averageScore);
+        collectiond.updateStudent(ids, averageScoreD);
+        System.out.println("Информация обновлена о студенте с номером зачетной книжки " + ids );
+    }
+
+
+    public static void main(String[] args) throws IOException {
+       launch(args);
+    }
+}
---
+++ RMIProject/src/main/java/rmi/dao/CrudImpl.java
@@ -0,0 +1,123 @@
+package rmi.dao;
+
+import rmi.XMLParser;
+import rmi.model.Student;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.*;
+
+public class CrudImpl extends UnicastRemoteObject implements ICrudCollection {
+    private List<Student> studentList;
+    private XMLParser xml = new XMLParser();
+    public CrudImpl() throws RemoteException {
+
+        studentList =  xml.readListStudent();
+       // studentList = new ArrayList<>();
+      //  studentList.add(new Student(1, "Alex", "Stasov", Student.Department.AppliedMathematics, 4.0));
+
+       // studentList.add(new Student(2, "Anton", "Deverin", Student.Department.AppliedMathematics, 4.9));
+       // studentList.add(new Student(3, "Anton", "Barsukov", Student.Department.InformationalRadiosystems, 4.5));
+       // studentList.add(new Student(4, "Dima", "Bozhenkinn", Student.Department.AppliedMathematics, 4.2));
+       // studentList.add(new Student(5, "Andre", "Stasov", Student.Department.AppliedMathematics, 4.4));
+    }
+
+
+    @Override
+    public void addSt(Student student) throws RemoteException {
+
+        studentList.add(student);
+        xml.addStudent(student);
+    }
+
+    @Override
+
+    public List<Student> getAll() throws RemoteException {
+        for (Student s : studentList) {
+            System.out.println(s);
+        }
+        return studentList;
+    }
+
+    @Override
+    public List<Student> findBySurname(String surname) throws RemoteException {
+        List<Student> res = new ArrayList<>();
+        for (Student s : studentList) {
+            if (s.getSurname().equals(surname)) {
+                System.out.println(s);
+                res.add(s);
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public List<Student> findByName(String name) throws RemoteException {
+        List<Student> res = new ArrayList<>();
+        for (Student s : studentList) {
+            if (s.getName().equals(name)) {
+                System.out.println(s);
+                res.add(s);
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public List<Student> findByDepartment(Student.Department department) throws RemoteException {
+        List<Student> res = new ArrayList<Student>();
+
+        for (Student s : studentList) {
+            if (s.getDepartmet().equals(department)) {
+                System.out.println("Account found");
+                res.add(s);
+            } else {
+                System.out.println("Account not found");
+            }
+        }
+
+        return res;
+    }
+
+    @Override
+    public Student findByGradebookNumber(long number) throws RemoteException {
+        Student res = new Student();
+        for (Student s : studentList) {
+            if (s.getGradebookNumber() == (number)) {
+                res = s;
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public List<Student> findWhosScoreGreater(double minScore) throws RemoteException {
+        List<Student> res = new ArrayList<>();
+        for (Student s : studentList) {
+            if (s.getAverageScore() >= (minScore)) {
+                System.out.println(s);
+                res.add(s);
+            }
+        }
+        return res;
+    }
+
+
+    @Override
+    public void removeStudent(long id) throws RemoteException {
+        studentList.removeIf(p -> p.getGradebookNumber() == (id));
+        xml.removeStudent(id);
+    }
+
+    @Override
+    public void updateStudent(long id, double s) throws RemoteException {
+        for (Student t : studentList) {
+            if (t.getGradebookNumber() == (id)) {
+                System.out.println(t);
+                t.setAverageScore(s);
+                System.out.println(t);
+                xml.updateStudent(id, s);
+            }
+        }
+    }
+}
